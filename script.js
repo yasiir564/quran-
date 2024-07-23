91d5111b46cb406462bbec1a86e83b5d49f1225c
yasiir564 <160410948+yasiir564@users.noreply.github.com>
2024-07-23 01:26:31 -1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(error => {
+        console.log('ServiceWorker registration failed: ', error);
+    });
+}
