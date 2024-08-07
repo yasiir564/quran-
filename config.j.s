ec0918928435169600a9fb5804f8bc76990e0bfe
yasiir564 <160410948+yasiir564@users.noreply.github.com>
2024-08-06 19:42:22 -1200

    Update and rename config.php to config.j.s

---
+++ config.j.s
@@ -0,0 +1,10 @@
+module.exports = {
+    POSTGRES_URL: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    POSTGRES_PRISMA_URL: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15",
+    POSTGRES_URL_NO_SSL: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb",
+    POSTGRES_URL_NON_POOLING: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    POSTGRES_USER: "default",
+    POSTGRES_HOST: "ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech",
+    POSTGRES_PASSWORD: "XQd7YMe4zOWb",
+    POSTGRES_DATABASE: "verceldb"
+};
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', 'YASSIR');
-define('DB_USER', 'YASSIN');
-define('DB_PASS', 'Munamuna12@');
-define('DB_NAME', 'Quran pro');
-
-// Other configuration settings
-?>
