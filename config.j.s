489b8f9546be4907e3aa70e4e93b569e970945c3
yasiir564 <160410948+yasiir564@users.noreply.github.com>
2024-08-06 19:55:21 -1200

    Update config.j.s

--- config.j.s
+++ config.j.s
@@ -1,10 +1,14 @@
 module.exports = {
-    POSTGRES_URL: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
-    POSTGRES_PRISMA_URL: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15",
-    POSTGRES_URL_NO_SSL: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb",
-    POSTGRES_URL_NON_POOLING: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
-    POSTGRES_USER: "default",
-    POSTGRES_HOST: "ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech",
-    POSTGRES_PASSWORD: "XQd7YMe4zOWb",
-    POSTGRES_DATABASE: "verceldb"
+  POSTGRES_URL: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  POSTGRES_PRISMA_URL: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15",
+  POSTGRES_URL_NO_SSL: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb",
+  POSTGRES_URL_NON_POOLING: "postgres://default:XQd7YMe4zOWb@ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  POSTGRES_USER: "default",
+  POSTGRES_HOST: "ep-soft-sea-a2yblote-pooler.eu-central-1.aws.neon.tech",
+  POSTGRES_PASSWORD: "XQd7YMe4zOWb",
+  POSTGRES_DATABASE: "verceldb",
+  JWT_SECRET: "your_jwt_secret",
+  EMAILJS_USER_ID: "your_emailjs_user_id",
+  EMAILJS_SERVICE_ID: "your_emailjs_service_id",
+  EMAILJS_TEMPLATE_ID: "your_emailjs_template_id"
 };
